5f42f0a998cb9d437edc981fbe5b3cc9676570d9
mchang0506@gmail.com <mchang0506@gmail.com>
2024-08-09 15:28:16 -0400

    API Calls practice

---
+++ 3rdPractice/apiCalls.py
@@ -0,0 +1,14 @@
+import requests
+
+#MTG API Documentation
+#URL: https://docs.magicthegathering.io/#documentationgetting_started
+
+# The API endpoint
+url = "https://api.magicthegathering.io/v1/cards/386616"
+
+# A GET request to the API
+response = requests.get(url)
+
+# Print the response
+response_json = response.json()
+print(response_json)
